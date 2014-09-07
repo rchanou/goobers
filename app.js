6908c258851470d6b27d307973989aa858e976d3
rchanou <rchanou@gmail.com>
2014-09-07 00:30:42 -0700

    skeleton of view and server store

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+var GAME_TIME = 100;
+var COUNTDOWN_TIME = 3;
+
 var ws = require('ws');
 var wsServer = ws.Server;
 var http = require('http');
 var express = require('express');
 var app = express();
+var _ = require('underscore');
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
@@ -14,12 +18,90 @@ server.listen(app.get('port'), function(){
 
 var wss = new wsServer({server: server});
 
-wss.on('connection', function(socket){
-	var id = setInterval(function(){
-		socket.send(JSON.stringify(new Date()));
-	}, 1000);
+
+// FUNCTIONS
+var convertLevelMap = function(levelMap){
+	var level = {
+		walls: [],
+		playerStarts: [],
+		coins: []
+	};
 	
-	socket.on('close', function(){
-		clearInterval(id);
+	levelMap.forEach(function(tileRow, y){
+		for (var x = 0; x < tileRow.length; x++){
+			switch (tileRow.charAt(x)){
+				case 'x':
+					level.walls.push({x: x, y: y});
+					break;
+				case '@':
+					level.playerStarts.push({x: x, y: y});
+					break;
+			}		
+		}
+	});
+	
+	return level;
+};
+
+
+// STORE
+
+var testLevelMap = [
+	'xxxxxxxxxx',
+	'x@      @x',
+	'x        x',
+	'x        x',
+	'x        x',
+	'x@      @x',
+	'xxxxxxxxxx'
+];
+
+var goobers = new (function(){
+	this.games = [];
+	this.addGame = function(level){
+		this.games.push({
+			id: Math.random().toString(),
+			players: [],
+			level: convertLevelMap(testLevelMap),
+			timeLeft: GAME_TIME,
+			startCountdown: COUNTDOWN_TIME
+		});
+	}.bind(this);
+	
+	this.players = [];
+	this.addPlayer = function(playerId){
+		this.players.push({
+			id: playerId,
+			coins: 0,
+			gamesPlayed: []
+		});
+	}.bind(this);
+	
+	this.addPlayerToGame = function(player){
+		var latestGame = _.last(this.games);
+	
+		if (latestGame.players.length >= latestGame.level.playerStarts.length){
+			this.addGame();
+		}
+		
+		_.last(this.games).players.push({
+			id: player.id,
+			coins: 0
+		});
+	}.bind(this);
+})();
+
+goobers.addGame();
+
+// WEBSOCKETS
+wss.on('connection', function(socket){
+	var playerId = Math.random().toString();
+	
+	goobers.addPlayer(playerId);
+		
+	socket.send(JSON.stringify(convertLevelMap(testLevelMap)));
+	
+	socket.on('close', function(e){
+		console.log(e);
 	});
 });
--- public/goobers.jsx
+++ public/goobers.jsx
@@ -1,6 +1,15 @@
 /** @jsx React.DOM */
 
 var Goobers = React.createClass({
+	socket: null,
+	getDefaultProps: function(){
+		return {
+			scale: 40
+		};
+	},
+	k: function(){
+		return this.props.scale;
+	},
 	getInitialState: function(){
 		return {
 			currentPlayerId: null,
@@ -9,6 +18,7 @@ var Goobers = React.createClass({
 				players: [
 					{ id: null, coins: 0, pos: {x:null, y: null} }
 				],
+				playersToStart: null,
 				level: {
 					id: null,
 					walls: [{x: null, y: null}],
@@ -19,16 +29,54 @@ var Goobers = React.createClass({
 			}
 		};
 	},
-	componentDidMount: function(){
+	login: function(){
 
+	},
+	componentDidMount: function(){
+	    var host = location.origin.replace(/^http/, 'ws');
+		console.log(host);
+	    this.socket = new WebSocket(host);
+	    this.socket.onmessage = function(event){
+     		console.log(JSON.parse(event.data));
+	    };
+		document.onkeydown = function(){
+
+		};
+	},
+	checkGameReady: function(){
+		return this.state.game.players.length >= playersToStart;
+	},
+	wallRects: function(){
+		return this.state.game.level.walls.map(function(wall, i){
+			return <rect key={'wall'+i} fill='grey'
+				x={wall.x*this.k()} y={wall.y*this.k()}
+				width={this.k()} height={this.k()} />;
+		});
+	},
+	coinCircles: function(){
+		return this.state.game.level.coins.map(function(coin, i){
+			return <circle key ={'coin'+i} fill='yellow'
+				cx={(coin.x+0.5)*this.k()} cy={(coin.y+0.5)*this.k()}
+				r={this.k()/3} />;
+		});
+	},
+	getMaxWall: function(axis){ // axis is 'x' or 'y'
+		return Math.max.apply(null, _.pluck(this.state.game.level.walls, axis));
 	},
 	render: function(){
 		if (this.state.currentPlayerId === null){
 			return <div>Logging into server...</div>;
 		}
 
+		if (!this.checkGameReady()){
 
+		}
 
+		return <div>
+			<svg width={this.getMaxWall('x')} height={this.getMaxWall('y')}>
+				{this.wallRects()}
+			</svg>
+		</div>;
 		//return <Game />;
 	}
 });
